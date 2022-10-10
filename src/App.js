37bb658864e221fca0b7d2de78c7fc681e1edbd5
Yaser Odat <yaserodat@outlook.com>
2022-10-10 17:09:58 +0000

    ready

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import DeveloperForm from '../src/components/DeveloperForm'
+function App() {
+  return (
+    <div className="App">
+      <DeveloperForm/>
+    </div>
+  );
+}
+
+export default App;
